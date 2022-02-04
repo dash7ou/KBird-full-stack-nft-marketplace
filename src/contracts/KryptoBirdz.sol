47dc19c62cb96f871fba0df9b7a0e02b56363d4b
dash7ou <fxfddfno@gmail.com>
2022-02-04 14:40:42 -0500

    some improvments

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -5,12 +5,17 @@ import "./ERC721Connector.sol";
 
 contract KryptoBird is ERC721Connector {
     string[] public kryptoBirdz;
+    mapping(string => bool) private _kryptoBirdExists;
 
     constructor() ERC721Connector("KryptoBird", "KBIRDZ") {}
 
-    function mint(string memory _kryptoBirdz) public {
-        kryptoBirdz.push(_kryptoBirdz);
+    function mint(string memory _kryptoBird) public {
+        require(!_kryptoBirdExists[_kryptoBird], "Krypto Bird already exists!");
+
+        kryptoBirdz.push(_kryptoBird);
         uint256 _id = kryptoBirdz.length - 1;
         _mint(msg.sender, _id);
+
+        _kryptoBirdExists[_kryptoBird] = true;
     }
 }
