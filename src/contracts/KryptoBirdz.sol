75b7b8f455346b4152234a612bea38a5e216093a
dash7ou <fxfddfno@gmail.com>
2022-02-04 14:32:35 -0500

    mint function with main contract

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,5 +4,13 @@ pragma solidity ^0.8.0;
 import "./ERC721Connector.sol";
 
 contract KryptoBird is ERC721Connector {
+    string[] public kryptoBirdz;
+
     constructor() ERC721Connector("KryptoBird", "KBIRDZ") {}
+
+    function mint(string memory _kryptoBirdz) public {
+        kryptoBirdz.push(_kryptoBirdz);
+        uint256 _id = kryptoBirdz.length - 1;
+        _mint(msg.sender, _id);
+    }
 }
