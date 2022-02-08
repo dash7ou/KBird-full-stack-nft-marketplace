04517905cbb9da68684bf57bd91c0c317e545d85
dash7ou <fxfddfno@gmail.com>
2022-02-08 14:35:15 -0500

    complete the contract

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -6,7 +6,7 @@ import "./ERC721.sol";
 contract ERC721Enumerable is ERC721 {
     uint256[] private _allTokens;
 
-    // mapping from tokenId to position in _allToken array
+    // mapping from tokenId to position in _allTokenss array
     mapping(uint256 => uint256) private _allTokensIndex;
 
     // mapping of owner to list of all owner token ids
@@ -17,11 +17,32 @@ contract ERC721Enumerable is ERC721 {
 
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
-        _addTokenToTotalSupply(tokenId);
+        _addTokenToAllTokenEnumeration(tokenId);
+        _addTokenToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokenToTotalSupply(uint256 tokenId) private {
+    function _addTokenToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
+    }
+
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), "global index is out of bound!");
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        public
+        view
+        returns (uint256)
+    {
+        require(index < balanceOf(owner), "owner index is out of bound!");
+        return _ownedTokens[owner][index];
     }
 
     function totalSupply() public view returns (uint256) {
