5f02ef2539a3eb53f21df4af5b5523a3a2511ffc
dash7ou <fxfddfno@gmail.com>
2022-02-09 14:16:30 -0500

    some shit

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -8,11 +8,20 @@ contract ERC721 {
         uint256 indexed tokenId
     );
 
+    event Approval(
+        address indexed owner,
+        address indexed approved,
+        uint256 indexed tokenId
+    );
+
     // Mapping from tokenId to the owner.
     mapping(uint256 => address) private _tokenOwner;
 
     // Mapping from owner to the number of token he has.
     mapping(address => uint256) private _OwnedTokenCounter;
+
+    // Mapping from token ID to the approved address
+    mapping(uint256 => address) private _tokenApprovals;
 
     function _exists(uint256 tokenId) internal view returns (bool) {
         address owner = _tokenOwner[tokenId];
@@ -39,10 +48,36 @@ contract ERC721 {
         // }
     }
 
-    function ownerOf(uint256 _tokenId) external view returns (address) {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns (address) {
         address owner = _tokenOwner[_tokenId];
         require(owner != address(0), "Invalid owner address");
 
         return owner;
+    }
+
+    function approve(address _to, uint256 _tokenId) public {
+        address owner = ownerOf(_tokenId);
+        require(_to != owner, "Approval to current owner");
+        require(msg.sender == owner, "Current caller is not the owner");
+        _tokenApprovals[_tokenId] = _to;
+        emit Approval(owner, _to, _tokenId);
+    }
+
+    function isApprovedOrOwner(address spender, uint256 tokenId)
+        internal
+        view
+        returns (bool)
+    {
+        require(!_exists(tokenId), "Token not found!");
+        address owner = ownerOf(tokenId);
+        return (owner == spender);
     }
 }
