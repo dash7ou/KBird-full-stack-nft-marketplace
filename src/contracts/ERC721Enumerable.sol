2dbfc144aaa5cbdd0d9b962b4380167212da9368
dash7ou <fxfddfno@gmail.com>
2022-02-11 13:15:56 -0500

    enumerable interface

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./ERC721.sol";
+import "./interfaces/IERC721Enumerable.sol";
 
-contract ERC721Enumerable is ERC721 {
+contract ERC721Enumerable is IERC721Enumerable, ERC721 {
     uint256[] private _allTokens;
 
     // mapping from tokenId to position in _allTokenss array
