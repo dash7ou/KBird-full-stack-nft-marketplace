5bf0cd5cf3b0e987cfd71ccb1c534b80bb9b2aa7
dash7ou <fxfddfno@gmail.com>
2022-02-18 14:53:31 -0500

    fix lib issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+import { useEffect,useState  } from "react";
+import Web3 from "web3";
+import detectedEthereumProvider from "@metamask/detect-provider";
+// import  {Provider} from "@ethersproject/abstract-provider";
+// import Provider from "@ethersproject/providers";
+
+import KryptoBird from "./abis/KryptoBird.json";
+
 import './App.css';
 
+
 function App() {
+  const [providerApp, setProviderApp]: any = useState(null)
+
+  useEffect(()=>{
+    const loadingData = async ()=>{
+      await loadWeb3()
+      const x = new Web3(providerApp);
+      console.log(x)
+    } 
+    loadingData()
+  }, [])
+
+  const loadWeb3 = async (): Promise<any> =>{
+    try{
+      const provider = await detectedEthereumProvider();
+      setProviderApp(provider)
+
+      if(provider){
+        console.log("ethereum wallet connected!");
+
+      }else{
+        console.log("no ethereum walllet connected!");
+      }
+    }catch(err: any){
+
+    }
+  }
+
   return (
    <div></div>
   );
 }
 
+ 
 export default App;
