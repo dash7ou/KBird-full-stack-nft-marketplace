496cbd536b07808bb3a884adff0f42fd608f0477
dash7ou <fxfddfno@gmail.com>
2022-02-19 10:40:35 -0500

    accounts and ui

--- src/App.tsx
+++ src/App.tsx
@@ -10,35 +10,60 @@ import './App.css';
 
 
 function App() {
-  const [providerApp, setProviderApp]: any = useState(null)
+  // const [web3, setWeb3]: any = useState(null);
+  const [accounts, setAccounts] = useState<Array<string>>([]);
 
   useEffect(()=>{
     const loadingData = async ()=>{
-      await loadWeb3()
-      const x = new Web3(providerApp);
-      console.log(x)
-    } 
+      const web3 = await loadWeb3()
+      await loadBlockchainData(web3)
+    }
+
     loadingData()
   }, [])
 
   const loadWeb3 = async (): Promise<any> =>{
     try{
-      const provider = await detectedEthereumProvider();
-      setProviderApp(provider)
+      const provider : any = await detectedEthereumProvider();
+      const web3 = new Web3(provider);
+      // setWeb3(web3);
 
       if(provider){
         console.log("ethereum wallet connected!");
-
+        return web3
       }else{
         console.log("no ethereum walllet connected!");
       }
-    }catch(err: any){
+    }catch(err: any){ 
 
     }
   }
 
+
+  const loadBlockchainData = async (web3: any): Promise<any> => {
+    try{
+      const accounts  = await web3.eth.getAccounts();
+      setAccounts(accounts)
+    }catch(err: any){ 
+    }
+  }
+
   return (
-   <div></div>
+   <div>
+     <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <div className="navbar-brand col-sm-3 col-md-3 mr-0" style={{color: "white"}}>
+          Krypto Birdz NFTs (Non Fungible Tokens)
+        </div>
+
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-block">
+            <small className="text-white">
+              {accounts[0]}
+            </small>
+          </li>
+        </ul>
+     </nav>
+   </div>
   );
 }
 
